b0461be2a7ffad4a0595de97368ec9dbab96d55c
Natarajan-rangasamy <natraj2431@gmail.com>
2023-08-16 17:51:42 +0530

    working on it

---
+++ script.js
@@ -0,0 +1,15 @@
+function test (){
+   var btn = document.createElement("button");
+    btn.innerText = " hello ";
+    document.getElementById('card').innerHTML = 'java'
+    
+}
+
+function school(){
+    var sch = document.getElementById("school");
+    sch.classList.toggle("schoold");
+}
+function college(){
+    var sch = document.getElementById("college");
+    sch.classList.toggle("colleged");
+}
